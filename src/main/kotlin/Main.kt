2f35afd67bb74ad54a22fc02506aaea810d38be9
Sven Rahn <apps.randombyte@gmail.com>
2023-09-25 17:09:42 +0200

    Start with new UI

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,46 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+
+private class AppState {
+    var settingsOpened by mutableStateOf(false)
+    var learningOpened by mutableStateOf(false)
+
+    var oscPort by mutableStateOf(7700)
+}
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+    val state by remember { mutableStateOf(AppState()) }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Row {
+            TextField(
+                value = state.oscPort.toString(),
+                onValueChange = { it.toIntOrNull()?.also { port -> state.oscPort = port } },
+                label = { Text("Port") },
+            )
         }
     }
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(
+        onCloseRequest = ::exitApplication,
+        state = WindowState(width = 800.dp, height = 400.dp),
+        title = "Baustella Light Control",
+        icon = painterResource("new-moon.png"),
+        resizable = false,
+    ) {
         App()
     }
 }
