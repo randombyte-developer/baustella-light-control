19706ef2c71bfbc62aa06b3223cf7d9aee340dd2
Sven Rahn <apps.randombyte@gmail.com>
2023-10-03 20:43:51 +0200

    Add fat jar build

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,22 @@ dependencies {
     implementation("com.illposed.osc:javaosc-core:0.8")
 }
 
+val fatJar = task("fatJar", type = Jar::class) {
+    archiveBaseName.set("${project.name}-fat")
+    manifest {
+        attributes["Main-Class"] = "de.randombyte.baustellalightcontrol.MainKt"
+    }
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    with(tasks.jar.get() as CopySpec)
+}
+
+tasks {
+    "build" {
+        dependsOn(fatJar)
+    }
+}
+
 compose.desktop {
     application {
         mainClass = "de.randombyte.baustellalightcontrol.MainKt"
